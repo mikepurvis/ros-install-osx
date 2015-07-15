ba6f9ca18759e90e79c93789b7b5b855dcd041d9
Mike Purvis <mpurvis@clearpathrobotics.com>
2015-07-15 16:32:48 -0400

    Genericize distro, install path, configuration.

--- install-ros.sh
+++ install-ros.sh
@@ -1,15 +1,30 @@
+ROS_DISTRO=indigo
+ROS_CONFIGURATION=desktop_full
+ROS_INSTALL_DIR=/opt/ros/${ROS_DISTRO}
 
-# Homebrew (if you haven't yet got it)
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo export PATH='/usr/local/bin:$PATH' >> ~/.bash_profile
-source .bash_profile
-brew doctor
-brew update
+# Homebrew
+if hash brew 2>/dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo export PATH='/usr/local/bin:$PATH' >> ~/.bash_profile
+  source .bash_profile
+  brew doctor
+  brew update
+fi
+
+# XQuartz
+if hash xquartz 2>/dev/null; then
+  brew install caskroom/cask/brew-cask
+  brew cask install xquartz
+  echo "Log out and in to finalize XQuartz setup."
+  exit 0
+fi
 
 # Brewed Python
-brew install python
-mkdir -p ~/Library/Python/2.7/lib/python/site-packages
-echo "$(brew --prefix)/lib/python2.7/site-packages" >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
+if [ $(which python) != "/usr/local/bin/python" ]; then
+  brew install python
+  mkdir -p ~/Library/Python/2.7/lib/python/site-packages
+  echo "$(brew --prefix)/lib/python2.7/site-packages" >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
+fi
 
 # Homebrew taps for prerequisites
 brew tap ros/deps
@@ -21,7 +36,7 @@ brew tap homebrew/science
 brew install cmake libyaml lz4 assimp
 brew install boost --with-python
 brew install opencv --with-qt --with-eigen --with-tbb
-brew install ogre  # Ogre v1.7
+brew install ogre
 brew install gazebo5
 
 # ROS infrastructure tools
@@ -30,21 +45,22 @@ sudo rosdep init
 rosdep update
 
 # ROS Indigo Source Install
-mkdir indigo_desktop_ws && cd indigo_desktop_ws
-rosinstall_generator desktop_full --rosdistro indigo --deps --tar > indigo.rosinstall
-rosinstall_generator rviz --rosdistro hydro --tar >> indigo.rosinstall  # Version of rviz from Hydro
-wstool init -j8 src indigo.rosinstall
+WS=${ROS_DISTRO}_${ROS_CONFIGURATION}_ws
+mkdir $WS && cd $WS
+rosinstall_generator ${ROS_CONFIGURATION} --rosdistro ${ROS_DISTRO} --deps --tar > ${WS}.rosinstall
+#rosinstall_generator rviz --rosdistro hydro --tar >> ${WS}.rosinstall  # Version of rviz from Hydro
+wstool init -j8 src ${WS}.rosinstall
 
 # Package dependencies.
-rosdep install --from-paths src --ignore-src --rosdistro indigo -y --as-root pip:no --skip-keys="ogre gazebo"
+rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y --as-root pip:no --skip-keys="ogre gazebo"
 
 # Parallel build
-sudo mkdir -p /opt/ros/indigo
-sudo chown $USER /opt/ros/indigo
-catkin config --install  --install-space /opt/ros/indigo
+sudo mkdir -p ${ROS_INSTALL_DIR}
+sudo chown $USER ${ROS_INSTALL_DIR}
+catkin config --install  --install-space ${ROS_INSTALL_DIR}
 catkin build \
   -DCMAKE_BUILD_TYPE=Release \
   -DPYTHON_LIBRARY=$(python -c "import sys; print sys.prefix")/lib/libpython2.7.dylib \
   -DPYTHON_INCLUDE_DIR=$(python -c "import sys; print sys.prefix")/include/python2.7
 
-source /opt/ros/indigo/setup.bash
+source ${ROS_INSTALL_DIR}/setup.bash
